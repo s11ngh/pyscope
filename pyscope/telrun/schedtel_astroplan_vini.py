14de6afd21cf1b21da5b50f961d21f73f0e21018
Vineet Singh <vineet.singh14204@gmail.com>
2025-05-22 02:56:55 -0500

    test run

--- pyscope/telrun/schedtel_astroplan_vini.py
+++ pyscope/telrun/schedtel_astroplan_vini.py
@@ -15,6 +15,11 @@ from astroplan.constraints import AtNightConstraint, AirmassConstraint
 from astroplan import ObservingBlock
 from astroplan.constraints import TimeConstraint
 from astropy import units as u
+from astroplan.scheduling import Transitioner
+from astroplan.scheduling import SequentialScheduler
+from astroplan.scheduling import Schedule
+from astroplan.scheduling import PriorityScheduler
+
 
 
 # obsever
@@ -56,9 +61,15 @@ m13_exp = 100*u.second
 n = 16
 blocks = []
 
+
+# half night start and end times
+# Define the time constraint for the first half-night
+# on July 6, 2016
 half_night_start = Time('2016-07-07 02:00')
 half_night_end = Time('2016-07-07 08:00')
 first_half_night = TimeConstraint(half_night_start, half_night_end)
+
+
 # Create ObservingBlocks for each filter and target with our time
 # constraint, and durations determined by the exposures needed
 for priority, bandpass in enumerate(['B', 'G', 'R']):
@@ -72,3 +83,51 @@ for priority, bandpass in enumerate(['B', 'G', 'R']):
                                         configuration = {'filter': bandpass},
                                         constraints = [first_half_night])
     blocks.append(b)
+
+
+
+
+
+
+# Initialize a transitioner object with the slew rate and/or the
+# duration of other transitions (e.g. filter changes)
+slew_rate = .8*u.deg/u.second
+transitioner = Transitioner(slew_rate,
+                            {'filter':{('B','G'): 10*u.second,
+                                       ('G','R'): 10*u.second,
+                                       'default': 30*u.second}})
+
+
+
+
+
+
+
+
+
+# Initialize the sequential scheduler with the constraints and transitioner
+seq_scheduler = SequentialScheduler(constraints = global_constraints,
+                                    observer = apo,
+                                    transitioner = transitioner)
+# Initialize a Schedule object, to contain the new schedule
+sequential_schedule = Schedule(noon_before, noon_after)
+
+# Call the schedule with the observing blocks and schedule to schedule the blocks
+seq_scheduler(blocks, sequential_schedule)
+
+
+
+
+# Initialize the priority scheduler with the constraints and transitioner
+prior_scheduler = PriorityScheduler(constraints = global_constraints,
+                                    observer = apo,
+                                    transitioner = transitioner)
+# Initialize a Schedule object, to contain the new schedule
+priority_schedule = Schedule(noon_before, noon_after)
+
+# Call the schedule with the observing blocks and schedule to schedule the blocks
+prior_scheduler(blocks, priority_schedule)
+
+
+print("nigros")
+print(priority_schedule.to_table())
